fab2782a2c230a34e580b59808c08ce9ac8cbefb
N00234646@iadt.ie <N00234646@iadt.ie>
2024-04-13 18:24:32 +0100

    Added PHP view functionality

--- index.php
+++ index.php
@@ -4,16 +4,31 @@ require_once "./category.php";
 require_once "./location.php";
 require_once "./story.php";
 
-// $stories = Story::findAll($options = array('limit' => 2, 'offset' => 2));
+$stories_all = Story::findAll($options = array('limit' => 2, 'offset' => 2));
 
-// $authorId = 7;
-// $stories = Story::findByAuthor($authorId, $options = array('limit' => 3, 'offset' => 2));
+$authorId = 7;
+$author = Story::findByAuthor($authorId, $options = array('limit' => 1, 'offset' => 0));
 
 $categoryId = 1;
-$stories = Story::findByCategory($categoryId, $options = array('limit' => 2, 'offset' => 0));
+$stories1 = Story::findByCategory($categoryId, $options = array('limit' => 2, 'offset' => 0));
 
-// $locationId = 8;
-// $stories = Story::findByLocation($locationId, $options = array('limit' => 4, 'offset' => 0));
+$categoryId = 2;
+$stories2 = Story::findByCategory($categoryId, $options = array('limit' => 1, 'offset' => 0));
+
+$categoryId = 4;
+$stories3 = Story::findByCategory($categoryId, $options = array('limit' => 3, 'offset' => 1));
+
+$categoryId = 5;
+$stories4 = Story::findByCategory($categoryId, $options = array('limit' => 4, 'offset' => 0));
+
+$categoryId = 3;
+$stories6 = Story::findByCategory($categoryId, $options = array('limit' => 2, 'offset' => 0));
+
+$categoryId = 3;
+$stories7 = Story::findByCategory($categoryId, $options = array('limit' => 1, 'offset' => 5));
+
+$locationId = 8;
+$location = Story::findByLocation($locationId, $options = array('limit' => 4, 'offset' => 0));
 
 ?>
 <!DOCTYPE html>
@@ -55,293 +70,90 @@ $stories = Story::findByCategory($categoryId, $options = array('limit' => 2, 'of
 	<section class="Articles">
 		<div class="container">
 			<div class="panel large width-8">
-
-				<div class="imageHolder">
-					<img src="images/9.jpg">
+				<?php foreach ($stories2 as $s) { ?>
+        		<div class="imageHolder">
+        		<img src="<?= $s->img_url ?>" />
 				</div>
-
 				<div class="content">
-					<h3><a href="single.php">OpenAI is adding new watermarks to DALL-E 3</a></h3>
-
-					<div class="authorGenre">
-						<p class="author">LESLIE JAMISON</p>
-						<p>|</p>
-						<p class="genre">PSYCHOLOGY</p>
-					</div>
+				<h2>
+					<a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a>
+				</h2>
+				<p><?= $s-> author_id ?></p>
 				</div>
-
+    			<?php } ?>
 			</div>
 			
 			<div class="width-4">
-			<!-- <div class="panel small">
-					<img src="images/15.jpg">
-
-
-					<div class="content">
-						<h3><a href="#">8 charts on technology used around the world</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							The concept of Impostor Syndrome has become ubiquitous. Critics, and
-							even the idea's originators, question its value.
-						</p>
-
-					</div>
-
-			</div>
-
-				<div class="panel small width-4">
-					<img src="images/Chotiner-xi-balloon.webp">
-
-					<div class="content">
-						<h3><a href="#">Beverly Hills Cop: Axel F Release Date Confirmed For July</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							The concept of Impostor Syndrome has become ubiquitous. Critics, and
-							even the idea's originators, question its value.
-						</p>
-
-					</div>
-
-
-				</div> -->
-				
-				<?php foreach ($stories as $s) { ?>
+				<?php foreach ($stories1 as $s) { ?>
         		<div class="panel small">
         		<img src="<?= $s->img_url ?>" />
     
         		<div class="content">
-				<h4><?= $s->headline ?></h4>
+				<h4><a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a></h4>
            		<div class=""><?= substr($s->article,0,200) ?> 
 				</div>
         		</div>
         		</div>
     			<?php } ?>
-				</div>
-
-			<div class="width-4 panel medium">
-
-				<div class="content">
-					<img src="images/23.jpg">
-					<div class="title">
-						<h3><a href="#">Henry Cavill says heading up the Warhammer 40,000 cinematic universe is 'the
-							greatest privilege of my professional career'
-						</a>
-					</h3>
-					</div>
-
-					<div class="authorGenre">
-						<p class="author">LESLIE JAMISON</p>
-						<p>|</p>
-						<p class="genre">PSYCHOLOGY</p>
-					</div>
-
-				</div>
-
 			</div>
 
-			<div class="width-4 panel medium">
 
-				<div class="content">
-					<img src="images/25.jpg">
-					<div class="title">
-						<h3><a href="#">Suicide Squad: Kill the Justice League review – straddles the brilliant and the banal
-						</a>
-					</h3>
-					</div>
-					<div class="authorGenre">
-						<p class="author">LESLIE JAMISON</p>
-						<p>|</p>
-						<p class="genre">PSYCHOLOGY</p>
-					</div>
-
-
+			<?php foreach ($stories3 as $s) { ?>
+				<div class="width-4 panel medium">
+        		<div class="content">
+        		<img src="<?= $s->img_url ?>" />
+        		<div class="title">
+				<h4><a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a></h4>
+        		</div>
+        		</div>
 				</div>
-
-			</div>
-
-			<div class="width-4 panel medium">
-				<div class="content">
-					<img src="images/16.jpg">
-					<div class="title">
-						<h3><a href="#">'‘Transformers: Age of Extinction’ Hits $1 Billion Globally. “The Biggest, most
-							spectacular movie you will see this year”-Michael Bay
-						</a>
-					</h3>
-					</div>
-					<div class="authorGenre">
-						<p class="author">LESLIE JAMISON</p>
-						<p>|</p>
-						<p class="genre">PSYCHOLOGY</p>
-					</div>
-				</div>
-
-			</div>
+    		<?php } ?>
 
 			<div class="bottom width-4">
-				<div class="panel mini">
-
-
-					<div class="content">
-						<h3><a href="#">How Vince McMahon Got TKO’d</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							When Vince McMahon stepped into the New York Stock Exchange on Jan. 23, he was on hand to
-							celebrate what was supposed to be a triumph for TKO Group Holdings, the company for which he
-							was executive chairman, and the owner of the WWE, the wrestling promotion that McMahon
-							turned into a multibillion-dollar business.
-						</p>
-
-					</div>
-
-
+			<?php foreach ($stories4 as $s) { ?>
+        		<div class="panel mini">
+        		<div class="content">
+				<h3><a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a></h3>
+				<div class="description">
+				<p><?= substr($s->article,0,100) ?> </p>
+        		</div>
+        		</div>
 				</div>
-
-				<div class="panel mini width-4">
-
-					<div class="content">
-						<h3><a href="#">Local Streaming Startup Zeam Hopes That John Stamos Can Introduce It to a
-								Crowded Market (Exclusive)</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							A startup called Zeam is betting that it can, and it plans to use Super Bowl Sunday and
-							actor John Stamos to help introduce itself to the market.
-						</p>
-
-					</div>
-
-
-				</div>
-
-				<div class="panel mini width-4">
-
-					<div class="content">
-						<h3><a href="#">Health Systems Are Improving Patient Outcomes With AI-Assisted Technology</a>
-						</h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							A better approach would be to focus on the opportunities of AI—healthcare being a great
-							example. AI could play a tremendous role in reducing clinician burden and improving patient
-							experience.
-						</p>
-
-					</div>
-
-
-				</div>
-
-				<div class="panel mini width-4">
-
-					<div class="content">
-						<h3><a href="#">Local Streaming Startup Zeam Hopes That John Stamos Can Introduce It to a
-								Crowded Market (Exclusive)</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							A startup called Zeam is betting that it can, and it plans to use Super Bowl Sunday and
-							actor John Stamos to help introduce itself to the market.
-						</p>
-
-					</div>
-
-
-				</div>
-
+    		<?php } ?>
 			</div>
 
-			<div class="panel large width-8">
-
-				<div class="mediumstories">
-					<div class="smallcontent">
-						<img src="images/16.jpg">
-						<h3><a href="#">'‘Transformers: Age of Extinction’ Hits $1 Billion Globally. “The Biggest, most
-								spectacular movie you will see this year”-Michael Bay</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							The concept of Impostor Syndrome has become ubiquitous. Critics, and
-							even the idea's originators, question its value.
-						</p>
-
-					</div>
-
-					<div class="smallcontent">
-						<img src="images/16.jpg">
-						<h3><a href="#">'‘Transformers: Age of Extinction’ Hits $1 Billion Globally. “The Biggest, most
-								spectacular movie you will see this year”-Michael Bay</a></h3>
-
-						<div class="authorGenre">
-							<p class="author">LESLIE JAMISON</p>
-							<p>|</p>
-							<p class="genre">PSYCHOLOGY</p>
-						</div>
-
-						<p class="description">
-							The concept of Impostor Syndrome has become ubiquitous. Critics, and
-							even the idea's originators, question its value.
-						</p>
-
-					</div>
+			<?php foreach ($stories6 as $s) { ?>
+				<div class="width-4 panel medium">
+        		<div class="content">
+        		<img src="<?= $s->img_url ?>" />
+        		<div class="title">
+				<h4><a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a></h4>
+        		</div>
+        		</div>
 				</div>
+    		<?php } ?>
 
-				<div class="imageHolder">
-					<img src="images/10.jpg">
+			<div class="panel large width-12">
+			<?php foreach ($stories7 as $s) { ?>
+        		<div class="imageHolder">
+        		<img src="<?= $s->img_url ?>" />
 				</div>
-
-				<div class="content">
-					<h3><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-							Years Later</a></h3>
-
-					<div class="authorGenre">
-						<p class="author">LESLIE JAMISON</p>
-						<p>|</p>
-						<p class="genre">PSYCHOLOGY</p>
-					</div>
-				</div>
-
-				<!-- <p class="description">
-						The concept of Impostor Syndrome has become ubiquitous. Critics, and
-						even the idea's originators, question its value.
-					</p> -->
+        		<div class="content">
+				<h3><a href="single.php?id=<?= $s->id ?>">
+						<?= $s->headline ?>
+					</a></h3>
+        		</div>
+    		<?php } ?>
 			</div>
 		</div>
 		</div>
@@ -381,59 +193,6 @@ $stories = Story::findByCategory($categoryId, $options = array('limit' => 2, 'of
 			<p>© 2024 GAMING MEDIA, LLC. ALL RIGHTS RESERVED</p>
 		</div>
 	</section>
-
-
-
-	<!-- Mini Articles -->
-	<!-- <section class="miniArticles">
-			<div class="container">
-				<div class="width-4">
-					<div class="content">
-						<h3><a href="#">SEX, LOVE, AND THE STATE OF ROM-COM</a></h3>
-						<p class="author">
-							BY VINSON CUNNINGHAM, NAOMI FRY, & ALEXANDRA SCHWARTZ
-						</p>
-					</div>
-				</div>
-
-				<div class="width-4">
-					<div class="content">
-						<h3><a href="#">MY FAVOURITE TIMES TO USE INCOGNITO MODE</a></h3>
-						<p class="author">BY JADE ORLANDO</p>
-					</div>
-				</div>
-
-				<div class="width-4">
-					<div class="content">
-						<h3><a href="#">A G.P.S ROUTE FOR MY ANXIETY</a></h3>
-						<p class="author">BY JESSE EISENBERG</p>
-					</div>
-				</div>
-
-				<div class="width-4">
-					<div class="content">
-						<h3>
-							<a href="#">A NATIONAL EXPERIMENT IN REFUGEE RESETTLEMENT</a>
-						</h3>
-						<p class="author">BY GERALDO CADAVA</p>
-					</div>
-				</div>
-
-				<div class="width-4">
-					<div class="content">
-						<h3><a href="#">WHAT'S BEHIND THE CHINESE SPY BALLOON</a></h3>
-						<p class="author">BY ISAAC CHOTINER</p>
-					</div>
-				</div>
-
-				<div class="width-4">
-					<div class="content">
-						<h3><a href="#">IS ARTIFICIAL LIGHT POISONING THE PLANET?</a></h3>
-						<p class="author">BY ADAM GOPNIK</p>
-					</div>
-				</div>
-			</div>
-		</section> -->
 </body>
 
 </html>
--- single.php
+++ single.php
@@ -1,20 +1,36 @@
 <?php
-require_once "./author.php";
-require_once "./category.php";
-require_once "./location.php";
-require_once "./story.php";
+ require_once "./author.php";
+ require_once "./category.php";
+ require_once "./location.php";
+ require_once "./story.php";
 
-// $stories = Story::findAll($options = array('limit' => 2, 'offset' => 2));
+ try {
+    if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+        throw new Exception("Invalid request method");
+    }
+ 
+    if(array_key_exists("id", $_GET)){
+        $id = $_GET["id"];
+        $story = Story::findById($id);
+        if ($story === null){
+            throw new Exception("Movie not found");
+        }
+    }
+    else {
+        throw new Exception("Missing parameter: Movie id");
+    }
+ 
+    if(session_status()=== PHP_SESSION_NONE){
+        session_start();
+    }
+}
+catch (Exception $ex) {
+    echo $ex->getMessage();
+    exit();
+}
 
-// $authorId = 7;
-// $stories = Story::findByAuthor($authorId, $options = array('limit' => 3, 'offset' => 2));
 
-$categoryId = 3;
-$stories = Story::findByCategory($categoryId, $options = array('limit' => 5, 'offset' => 0));
-
-// $locationId = 8;
-// $stories = Story::findByLocation($locationId, $options = array('limit' => 4, 'offset' => 0));
-
+$stories_all = Story::findAll($options = array('limit' => 1, 'offset' => 0));
 ?>
 
 <!DOCTYPE html>
@@ -57,102 +73,24 @@ $stories = Story::findByCategory($categoryId, $options = array('limit' => 5, 'of
 	</section>
 	<section class="Articles">
 		<div class="container">
-			<div class="width-10">
-				<div class="content">
-					<h3><a href="#">Transformers: Age of Extinction’ Hits $1 Billion Globally</a></h3>
-					<div class="imageHolder">
-						<img src="images/16.jpg">
+			<div class="width-12">
+					<?php foreach ($stories_all as $s) { ?>
+        			<div class="imageHolder">
+        				<img src="<?= $s->img_url ?>" />
 					</div>
-					<p class="author">
-						BY VINSON CUNNINGHAM, NAOMI FRY, & ALEXANDRA SCHWARTZ
-					</p>
-					<p>Transformers: Age of Extinction became the first film this year to pass $1 billion at the
-						global box office.</p>
-					<p>The Michael Bay film is the second picture in the giant robots franchise to hit those lofty
-						heights, after 2009’s “Transformers: Dark of the Moon.” The latest “Transformers” was propelled
-						by a record-breaking reception in China, where the film has earned north of $300 million, a new
-						high-water mark for the world’s second-largest movie market. Its cumulative total includes
-						$763.8 million internationally and $241.2 million at the domestic box office.</p>
-					<p>For the fourth film in the franchise, Paramount shook things up, upgrading original series star
-						Shia LaBeouf for Mark Wahlberg and shooting large parts of the movie in China in order to better
-						appeal to that burgeoning market.</p>
-					<p>The studio is making sure it doesn’t let its golden goose fly the coop. This week it announced a
-						three-year deal extension with Bay and his production company, Platinum Dunes.The fourth
-						installment of the Paramount/Hasbro-backed series, once again directed by Michael Bay, is
-						believed to possibly become the first film of 2014 to hit $100 million in its box-office debut.
-						Overseas, Age of Extinction has already earned $30 million, including record-setting Thursday
-						midnight previews in China ($3.5 million) and a stellar $5.5 million Thursday start in Russia,
-						the second biggest opening day of all time.</p>
-					<p>Read what top critics are saying about Transformers: Age of Extinction:</p>
-					<p>The Hollywood Reporter‘s film critic Clarence Tsui says in his review that “there’s a lot of
-						state-of-the-art 3D chicanery, and the film is a marked improvement over the wholesale inhuman
-						chaos of the last two installments, 2009’s Revenge of the Fallen and 2011’s Dark of the Moon.
-						But the bloat of this latest entry — at 165 minutes, the longest of the lot — suggests that
-						Michael Bay and his team are struggling to rejuvenate the whole premise. … It has neither the
-						first film’s sporadic comedic pleasures born of the interactions between its humans and robots,
-						nor does it attain the hyper-sensationalism that makes the second and third installments utterly
-						over-the-top showcases of gratuitous demolition.”</p>
-					<p>Besides being “dull,” the film includes odd allusions to directors like Stanley Kubrick and John
-						Ford, but a few digs at Bay himself for “cheap laughs,” and “even when the action switches to
-						Hong Kong — a twist designed to qualify the film for Chinese co-production status and a bigger
-						slice of the Asian box-office pie — the noise never abates.” Even more so, Wahlberg’s “troubled
-						interactions with Tessa [Peltz] and barbed exchanges with Shane [Reynor] are cliched or
-						underwritten, and his ability to morph into a bazooka-wielding warrior (“This alien gun can
-						really kick ass!”) limits his register further.” Tucci’s “clownish antics while racing for
-						survival in a Hong Kong tenement block are probably the highlight of the film.”</p>
-
-				</div>
-			</div>
-			<div class="content width-2">
-				<h3>More stories</h3>
-				<?php foreach ($stories as $s) { ?>
-        		<div class="imageHolder">
-        		<img src="<?= $s->img_url ?>" />
-				<h4><?= $s->headline ?></h4>
-           		<div class=""> 
-					<h4>Read more<h4>
-        		</div>
-				<br>
-        		</div>
-    			<?php } ?>
-				</div>
-
-				<!-- <h1>More stories</h1>
-				<div class="imageHolder">
-					<img src="images/10.jpg">
-				</div>
-				<h4><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-						Years Later
-					</a>
-				</h4>
-				<div class="imageHolder">
-					<img src="images/10.jpg">
-				</div>
-				<h4><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-						Years Later
-					</a>
-				</h4>
-				<div class="imageHolder">
-					<img src="images/10.jpg">
-				</div>
-				<h4><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-						Years Later
-					</a>
-				</h4>
-				<div class="imageHolder">
-					<img src="images/10.jpg">
-				</div>
-				<h4><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-						Years Later
-					</a>
-				</h4>
-				<div class="imageHolder">
-					<img src="images/10.jpg">
-				</div>
-				<h4><a href="#">Tom & Jerry's Critically Panned Live-Action Movie Gets Second Life On Netflix 3
-						Years Later
-					</a>
-				</h4> -->
+					<br>
+					<div class="content">
+						<h2>
+							<a href="single.php?id=<?= $s->id ?>">
+								<?= $s->headline ?>
+							</a>
+						</h2>
+						<br>
+						<div class=""><?= $s->article ?>
+						<br> 
+						<p><?= $s-> author_id ?></p>
+					</div>
+    				<?php } ?>
 			</div>
 		</div>
 	</section>
