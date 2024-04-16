b05b18eefadf1be449c32b97b8abdb352ada36e0
N00234646@iadt.ie <N00234646@iadt.ie>
2024-04-16 20:06:12 +0100

    Added Javascript code in file

--- single.php
+++ single.php
@@ -52,6 +52,7 @@ catch (Exception $ex) {
 	<link rel="stylesheet" href="CSS/footer.css" />
 	<link rel="stylesheet" href="CSS/main.css" />
 	<link rel="stylesheet" href="CSS/single.css" />
+	<script src="script.js"></script>
 	<title>single story</title>
 </head>
 
@@ -122,75 +123,6 @@ catch (Exception $ex) {
 		</div>
 	</section>
 
-	<script>
-		"use strict";
-
-		const userId = {
-			name:null,
-			identity:null,
-			image:null,
-			message: null,
-			date:null
-		}
-
-		const userComment = document.querySelector(".usercomment");
-		const publishBtn = document.querySelector("#publish");
-		const comments = document.querySelector(".comments");
-		const userName = document.querySelector(".user");
-
-		userComment.addEventListener("input", e => {
-			if(!userComment.value){
-				publishBtn.setAttribute("disabled", "disabled");
-				publishBtn.classList.remove("abled");
-			}
-			else{
-				publishBtn.removeAttribute("disabled");
-				publishBtn.classList.add("abled");
-			}
-
-		})
-
-		function addPost(){
-			console.log("the button works!");
-			if(!userComment.value) return;
-			userId.name = userName.value;
-			if(userId.name === "Anonymous"){
-				userId.identity = false;
-				userId.image = "anonymous.jpg";
-			}
-			else{
-				userId.identity = true;
-				userId.image = "user1.jpg";
-			}
-
-			userId.message = userComment.value;
-			userId.date = new Date().toLocaleString();
-			let published = 
-			`<div class="parents">
-				<img src="${userId.image}"
-				<div class="commentcontent">
-					<h1>${userId.name}<h1>
-					<p>${userId.message}</p>
-					<div class="engagements">
-						<img src="like.png">
-						<img src="share.png">
-					</div>
-					<span class= "date">${userId.date}</span>
-				</div>
-			 </div>
-			`;
-
-			comments.innerHTML += published;
-			userComment.value = "";
-
-			let commentsNum = document.querySelectorAll(".parents").length;
-			document.getElementById("comment").textContent = commentsNum; 
-		}
-
-		publishBtn.addEventListener("click", addPost)
-
-
-	</script>
 
 	<section class="footer">
 		<div class="footertitle">
